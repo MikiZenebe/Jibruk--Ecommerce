08fb3b19806b90fc84624d9c04856ad9f0ef1a04
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-02-24 15:50:47 +0300

    updated

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,26 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import Front from "../assets/Front.png";
+import { urlFor } from "../lib/client";
 
-export default function Banner() {
+export default function Banner({ bannerData }) {
   return (
     <div className=" p-14 justify-center items-center w-full ">
       <div className="flex flex-col  sm:px-8 md:flex-row ">
-        <div className="md:absolute md:right-10 md:max-w-[350px] md:mt-10 ">
-          <Image src={Front} alt="" className="sm:max-w-lg h-full " />
+        <div className="md:absolute md:right-10 md:max-w-[350px] md:mt-10 lg:right-28">
+          <img
+            src={urlFor(bannerData.image.asset._ref)}
+            alt=""
+            className="sm:max-w-[400px] h-full "
+          />
         </div>
 
         <div className="md:flex md:flex-col ">
           <h1 className="text-3xl font-bold mt-10 lg:text-[3rem]">
-            FEEL IT TO BELIVE IT
+            {bannerData.slogan}
           </h1>
-          <p className="mt-4 md:flex-wrap md:max-w-xl lg:text-2xl lg:mt-8">
-            Our most cushioned road-running shoe gives you extraordinary
-            comfort, supreme softness and lightweight support through every
-            mile.
+          <p className="mt-4 md:flex-wrap md:max-w-xl lg:text-2xl lg:mt-8 lg:font-normal">
+            {bannerData.desc}
           </p>
 
           <button className="lg:py-3 lg:text-[20px] lg:max-w-[120px] lg:mt-8 mt-6 flex justify-center border-2 rounded-lg bg-black text-white py-1 px-2 max-w-[80px] text-center ">
-            Explore
+            {bannerData.buttonText}
           </button>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import { Banner } from "../components";
+import { client } from "../lib/client";
 
-export default function Home() {
+//Fetch the Data
+export const getServerSideProps = async () => {
+  const BannerQuery = '*[_type == "banner"]'; //grab all item from banner
+  const bannerData = await client.fetch(BannerQuery); //fetch the banner items
+
+  return {
+    props: { bannerData },
+  };
+};
+
+export default function Home({ bannerData }) {
+  console.log(bannerData);
   return (
     <>
-      <div>
-        <Banner />
-      </div>
+      <Banner bannerData={bannerData.length && bannerData[0]} />
     </>
   );
 }
