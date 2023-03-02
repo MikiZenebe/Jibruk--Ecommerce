d285461ae471286047e90c8913d1cf547bf8243c
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-03-02 19:50:55 +0300

    updated

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,23 +9,23 @@ import {
 } from "react-icons/ai";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
-import CartAnim from "../assets/sale.gif";
 import Link from "next/link";
 
 function Cart() {
   const cartRef = useRef();
-  const { totalPrice, totalQty, cartItems, setShowCart } = useStateContext();
+  const { totalPrice, totalQty, cartItems, setShowCart, cartAdd } =
+    useStateContext();
 
   return (
     <div
       ref={cartRef}
       className="bg-[#00000066] w-full h-[100vh] fixed right-0 top-0 z-[100] flex justify-end"
     >
-      <div className="bg-white w-[350px] overflow-y-scroll relative">
+      <div className="bg-[#F1F2F6] w-[350px] overflow-y-scroll relative">
         <div className="pt-5 pl-4 flex items-center gap-2">
           <AiOutlineLeft cursor="pointer" onClick={() => setShowCart(false)} />
           <span className="font-semibold">Your Cart</span>
-          <span>(items)</span>
+          <span>({totalQty} items)</span>
         </div>
 
         {cartItems.length < 1 && (
@@ -44,18 +44,44 @@ function Cart() {
 
         <div>
           {cartItems.length >= 1 &&
-            cartItems.map((item) => {
-              <div>
-                <img src={urlFor(item.image[0])} alt="" />
+            cartItems.map((item) => (
+              <div key={item._id}>
+                <div
+                  style={{
+                    boxShadow: " 0px 70px 73px -33px rgba(0, 0, 0, 0.062)",
+                  }}
+                  className="flex justify-center items-center mt-10 ml-6 h-[130px]  gap-9 bg-white w-[300px]  rounded-lg relative"
+                >
+                  <div className="absolute right-2 top-2 hover:text-red-500 cursor-pointer card">
+                    <AiOutlineDelete />
+                  </div>
+                  <img
+                    className="card w-[60px] relative"
+                    src={urlFor(item.image[0])}
+                    alt=""
+                  />
 
-                <div>
-                  <div>
-                    <h5>{item.name}</h5>
-                    <h4>{item.price}</h4>
+                  <div className="flex flex-col text-gray-700 w-[180px]  mt-1">
+                    <h5 className="font-bold text-[14px]">{item.name}</h5>
+                    <h5 className="font-semibold">${item.price}</h5>
+
+                    <div className="mt-2 flex items-center gap-3 text-black">
+                      <p className="font-semibold ">Quantity</p>
+
+                      <span>
+                        <AiOutlineMinusCircle />
+                      </span>
+
+                      <span>{totalQty}</span>
+
+                      <span className="cursor-pointer">
+                        <AiOutlinePlusCircle onClick={() => cartAdd(item, 1)} />
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>;
-            })}
+              </div>
+            ))}
         </div>
       </div>
     </div>
