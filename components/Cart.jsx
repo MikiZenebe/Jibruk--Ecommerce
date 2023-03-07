6736147022c04221c6b0ba3ee5f74dd152ac76ed
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-03-07 20:33:59 +0300

    updated

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -128,7 +128,7 @@ function Cart() {
               <span className="justify-end">${totalPrice}</span>
             </div>
             <button
-              className="bg-black w-[150px] ml-24 py-1  text-white font-bold rounded-md card"
+              className="bg-black w-[150px] ml-24 py-1  text-white font-bold rounded-md hover:bg-gray-700 focus:bg-gray-700"
               onClick={handleCheckout}
             >
               Purchase
