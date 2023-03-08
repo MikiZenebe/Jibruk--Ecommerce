c57c3fbe8d8320839075dd3ffd29e61999034e3a
MikiZenebe <mikiyaszenebe10@gmail.com>
2023-03-08 21:51:26 +0300

    updated

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,11 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { MdFactCheck } from "react-icons/md";
 import { useRouter } from "next/router";
+import formatMoney from "../lib/formatMoney";
+import { card } from "../animation/CartAnimation";
+import { motion } from "framer-motion";
 
 //Display the success checkout
 const stripe = require("stripe")(
@@ -19,11 +23,16 @@ export async function getServerSideProps(params) {
 }
 
 function success({ order }) {
-  console.log(order);
+  const route = useRouter();
   return (
     <div className="h-[100vh] p-6">
       <div className="px-14 pb-12 bg-transparent flex justify-center items-center h-[100vh]">
-        <div className=" h-[500px] w-[500px] bg-white rounded-2xl">
+        <motion.div
+          variants={card}
+          animate="show"
+          initial="hidden"
+          className=" h-[500px] w-[500px] bg-white rounded-2xl"
+        >
           <div className="flex flex-col justify-center items-center p-6">
             <MdFactCheck size={70} className="text-green-400" />
             <h1 className="text-[30px] font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-300">
@@ -33,17 +42,45 @@ function success({ order }) {
 
           <div className="justify-center items-center flex flex-col ">
             <div className="flex gap-1">
-              <p className="font-medium text-gray-300">Customer Email</p> :{" "}
-              <span className="font-normal">
+              <p className="font-medium text-gray-400">Customer Email:</p>{" "}
+              <span className="font-normal text-gray-400">
                 {order.customer_details.email}
               </span>
             </div>
-          </div>
 
-          <div className="flex justify-center items-center py-4 font-medium text-gray-400">
-            ---------------------------------------------------------
+            <div className="flex justify-center items-center py-4 font-medium text-gray-400">
+              ---------------------------------------------------------
+            </div>
+
+            <div className="text-gray-400">
+              {order.line_items.data.map((item) => (
+                <div key={item.id}>
+                  <div className="flex justify-between gap-32 pb-6">
+                    <p className="font-semibold">Product:</p>{" "}
+                    <p>{item.description}</p>
+                  </div>
+
+                  <div className="flex justify-between gap-32 pb-6">
+                    <p className="font-semibold">Price:</p>{" "}
+                    <p>{formatMoney(item.price.unit_amount)}</p>
+                  </div>
+
+                  <div className="flex justify-between gap-32 pb-6">
+                    <p className="font-semibold">Quantity:</p>{" "}
+                    <p>{item.quantity}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <button
+              className="bg-black w-[150px] text-[15px] mt-4 py-2  text-white font-semibold rounded-md focus:bg-gray-700 card"
+              onClick={() => route.push("/")}
+            >
+              Continue Shopping
+            </button>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
